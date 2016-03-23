d201708e671d0d735b6ba78107510f8e0bffa432
allan <epicallan.al@gmail.com>
2016-03-23 05:04:40 +0300

    api server cleanup

--- api/actions/index.js
+++ api/actions/index.js
@@ -1,7 +1,2 @@
-export loadInfo from './loadInfo';
-export loadAuth from './loadAuth';
-export login from './login';
-export logout from './logout';
 export spotlight from './spotlight';
 export profile from './profile';
-export * as widget from './widget/index';
